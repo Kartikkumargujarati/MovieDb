f65a943734f511993289dc13ad816d23144009b2
Kartik Kumar Gujarati <kartikkumar.gujarati@rackspace.com>
2019-07-18 18:36:35 -0500

    Refactored to remove some of the out of the box code. App currently has 2 fragments that needed.

--- app/src/main/java/com/kartik/salesforcechallenege/ui/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/kartik/salesforcechallenege/ui/favorites/FavoritesViewModel.kt
@@ -1,10 +1,15 @@
-package com.kartik.salesforcechallenege.ui.dashboard
+/*
+ * Created by Kartik Kumar Gujarati on 7/18/19 6:33 PM
+ * Copyright (c) 2019 . All rights reserved.
+ */
+
+package com.kartik.salesforcechallenege.ui.favorites
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class FavoritesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
--- app/src/main/java/com/kartik/salesforcechallenege/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/kartik/salesforcechallenege/ui/search/SearchViewModel.kt
@@ -1,10 +1,15 @@
-package com.kartik.salesforcechallenege.ui.home
+/*
+ * Created by Kartik Kumar Gujarati on 7/18/19 6:33 PM
+ * Copyright (c) 2019 . All rights reserved.
+ */
+
+package com.kartik.salesforcechallenege.ui.search
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class SearchViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
