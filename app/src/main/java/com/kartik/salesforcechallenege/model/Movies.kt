f19c1a8b94b71a6aecc2a4c0454b8220d2e7e965
Kartik Kumar Gujarati <kartikkumar.gujarati@rackspace.com>
2019-07-18 19:54:19 -0500

    Added new data class for Movie/movieList.

---
+++ app/src/main/java/com/kartik/salesforcechallenege/model/Movies.kt
@@ -0,0 +1,38 @@
+/*
+ * Created by Kartik Kumar Gujarati on 7/18/19 7:10 PM
+ * Copyright (c) 2019 . All rights reserved.
+ */
+
+package com.kartik.salesforcechallenege.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+class Movies {
+    @Parcelize
+    data class MovieList(
+        @SerializedName("Search")
+        val movies: List<Movie?>?,
+        @SerializedName("totalResults")
+        val totalResults: String?
+    ) : Parcelable
+
+    @Entity(tableName = "movie_table")
+    @Parcelize
+    data class Movie(
+        @SerializedName("Poster")
+        val poster: String?,
+        @SerializedName("Title")
+        val title: String?,
+        @SerializedName("Type")
+        val type: String?,
+        @SerializedName("Year")
+        val year: String?,
+        @SerializedName("imdbID")
+        @PrimaryKey
+        val imdbID: String?
+    ) : Parcelable
+}
